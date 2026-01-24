84a45f0bd49c6061663817d4a0bfd6eed35e455e
SevenProxy <flapbraga@gmail.com>
2026-01-24 01:40:44 -0300

    feat(foot): Event onsubmit and oninput events have been added.

--- src/binare/foot.rs
+++ src/binare/foot.rs
@@ -1,11 +1,16 @@
 use yew::prelude::*;
+use web_sys::{
+    HtmlInputElement,
+    KeyboardEvent,
+    console,
+};
 use crate::system::WindowLucyRoot;
 use crate::system::PropsWindowLucy;
 
 #[component]
 pub fn Foot() -> Html {
-    let outputs = use_state(|| Vec::<String>::new());
-    let output = use_state(|| html! {<></>});
+    let output_history = use_state(|| Vec::<String>::new());
+    let input_value = use_state(|| String::new());
 
     let window_props_terminal = yew::props! {
         PropsWindowLucy {
@@ -13,6 +18,34 @@ pub fn Foot() -> Html {
             style_custom: "w-[600px] z-0".to_string(),
             sub_style: "bg-black overflow-y-auto overflow-x-hidden px-2 h-full text-white",
         }
+    };
+
+    let oninput = {
+        let input_value = input_value.clone();
+
+        Callback::from(move |e: InputEvent| {
+            let input: HtmlInputElement = e.target_unchecked_into();
+            input_value.set(input.value());
+        })
+    };
+
+    let onsubmit = {
+        let output_history = output_history.clone();
+        let input_value = input_value.clone();
+
+        Callback::from(move |e: SubmitEvent| {
+            e.prevent_default();
+
+            if input_value.is_empty() {
+                return;
+            }
+
+            let mut new_line = (*output_history).clone();
+            new_line.push(input_value.to_string());
+
+            output_history.set(new_line);
+            input_value.set(String::new());
+        })
     };
 
 
@@ -40,6 +73,28 @@ pub fn Foot() -> Html {
                     <p>{"⠀⠀⠀⠀⠀⢠⡠⠔⠋⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠙⠲⣿⣧"}</p>
                     <p>{"⢀⠔⠒⠀⠉⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠀⠉⠚⠤⢔"}</p>
                 </div>
+
+                <div>
+                    { for output_history.iter().map(|line| html! {
+                        <div class="w-full my-2">
+                            <div class="my-2">
+                                <div class="flex items-center">
+                                    <span class="text-pink-600">{"╭─"}</span>
+                                    <span class="text-pink-600">{"proxy"}</span>
+                                    <span>{"@"}</span>
+                                    <span class="text-pink-800">{"Skynet"}</span>
+                                    <span class="mx-1">{"in"}</span>
+                                    <span class="text-pink-600">{"/home"}</span>
+                                </div>
+                                <div class="flex items-center gap-1">
+                                    <span class="text-pink-600">{"╰─λ"}</span>
+                                    <p class="w-full text-white">{line.clone()}</p>
+                                </div>
+                            </div>
+                        </div>
+                    }) }
+                </div>
+
                 <div class="w-full my-2">
                     <div class="my-2">
                         <div class="flex items-center">
@@ -50,10 +105,10 @@ pub fn Foot() -> Html {
                             <span class="mx-1">{"in"}</span>
                             <span class="text-pink-600">{"/home"}</span>
                         </div>
-                        <div class="flex items-center gap-1">
+                        <form {onsubmit} class="flex items-center gap-1">
                             <span class="text-pink-600">{"╰─λ"}</span>
-                            <input class="bg-transparent border-0 w-full outline-none"/>
-                        </div>
+                            <input value={(*input_value).clone()} {oninput} autofocus=true class="bg-transparent border-0 w-full outline-none"/>
+                        </form>
                     </div>
                 </div>
             </div>
